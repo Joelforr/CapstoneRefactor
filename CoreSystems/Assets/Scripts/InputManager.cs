5726f4444f64136862ee26628d2c1df21f68287c
DESKTOP\Joel <joelforr@gmail.com>
2017-12-19 18:51:45 -0500

    Init

---
+++ CoreSystems/Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    public delegate PlayerState TransitionState(PlayerState currentState, Input input);
+    public event TransitionState TransitionHandler;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateState()
+    {
+        TransitionHandler();
+    }
+}
